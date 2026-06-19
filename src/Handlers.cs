d6767ef9740ffe23d09049829666a4d2542cd872
MidKnightXI <romain.bouguet@epitech.eu>
2026-06-19 12:58:56 +0200

    feat: adding base of handler routing

---
+++ src/Handlers.cs
@@ -0,0 +1,18 @@
+using Tools.Models;
+
+namespace Tools;
+
+public static class Handlers
+{
+    public static void Remove(PredictionResult predictionResult, double threshold)
+    {
+    }
+
+    public static void Move(PredictionResult predictionResult, double threshold)
+    {
+    }
+
+    public static void Copy(PredictionResult predictionResult, double threshold)
+    {
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -7,13 +7,49 @@ namespace Tools;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static void HandleAction(ParseResult result)
+    {
+        if (result.Errors.Count > 0)
+        {
+            foreach (var e in result.Errors)
+            {
+                Console.Error.WriteLine(e);
+            }
+            return;
+        }
+
+        var prediction = result.GetValue<PredictionResult>("prediction");
+        double threshold = result.GetValue<double>("--threshold");
+
+        if (prediction is null)
+        {
+            Console.Error.WriteLine("Argument 'prediction' cannot be null");
+            return;
+        }
+
+        switch (result.GetValue<Tool>("--tool"))
+        {
+            case Tool.Remove:
+                Handlers.Remove(prediction, threshold);
+                break;
+            case Tool.Move:
+                Handlers.Move(prediction, threshold);
+                break;
+            case Tool.Copy:
+                Handlers.Copy(prediction, threshold);
+                break;
+            case Tool.None:
+                return;
+        }
+        return;
+    }
+    private static int Main(string[] args)
     {
         var positionalOptions = new Argument<List<PredictionResult>>("prediction")
         {
             Description = "Result of the prediction made by the detection module.",
             Arity = ArgumentArity.ExactlyOne,
-            CustomParser = r => JsonSerializer.Deserialize(r.Tokens[0].Value, JsonContext.Default.ListPredictionResult) ?? []
+            CustomParser = r => JsonSerializer.Deserialize(r.Tokens[0].Value, JsonContext.Default.ListPredictionResult) ?? [],
         };
         var toolOptions = new Option<Tool>("--tool")
         {
@@ -25,14 +61,17 @@ internal class Program
         {
             Arity = ArgumentArity.ExactlyOne,
             Recursive = true,
-            Description = "Threshold that should be used to filter the scores. Only provide this option if the predictions contain a score."
+            Description = "Threshold that should be used to filter the scores. Only provide this option if the predictions contain a score.",
         };
 
         var rootCommand = new RootCommand("Tools module for Quadra.")
         {
             positionalOptions,
             toolOptions,
-            thresholdOptions
+            thresholdOptions,
         };
+        rootCommand.SetAction(HandleAction);
+        ParseResult parseResult = rootCommand.Parse(args);
+        return parseResult.Invoke();
     }
 }
